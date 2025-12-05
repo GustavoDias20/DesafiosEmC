e204152445e8f6be5ec216ec9bfa31ccc44c5f14
GustavoDias20 <gu2005dias@gmail.com>
2025-12-05 04:02:07 -0300

    first commit

---
+++ supertrunfoC/super_trunfo.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    //declarando as variaveis
+    char estado, codigoCarta[10], nomeCidade[50], estado2, codigoCarta2[10], nomeCidade2[50];
+    int populacao, pontosTuristicos, populacao2, pontosTuristicos2;
+    float area, pib, area2, pib2;
+
+    printf("Super Trunfo!\n Cadastro de cartas\n");
+    printf("Carta 01\n");
+
+    //cadastro do estado
+    printf("Entre com o nome do Estado da carta: (uma letra de 'A' a 'H')\n");
+    scanf(" %c", &estado);
+
+    //cadastro do codigo da carta
+    printf("Entre com o codigo da carta: (letra do estado + numero de 01 a 04, como exemplo A04)\n");
+    scanf(" %s", codigoCarta);
+    getchar(); //consome o \n que ficou no buffer
+
+    //cadastro do nome da cidade
+    printf("Entre com o nome da cidade\n");
+    fgets(nomeCidade,50,stdin); //fgets para conseguir pegar nomes que tenham espaço
+    
+    //com o fgets, ele lê o que é digitado completamente pelo teclado, então caso eu digite um nome e clique enter, ele vai receber
+    // "São Paulo\n" aonde o \n seria o enter do teclado, a gente pode remover esse \n em codigo
+
+    nomeCidade[strcspn(nomeCidade, "\n")] = 0;
+
+    //nesta linha a gente transforma o \n em \0, o que acaba com a leitura da cadeia de caracteres é sempre um \0
+
+    //cadastro da população
+    printf("Entre com o numero de habitantes\n");
+    scanf("%i", &populacao);
+
+    //cadastro da área em km quadrado
+    printf("Entre com a área em km quadrados\n");
+    scanf("%f", &area);
+
+    //cadastro do PIB
+    printf("Entre com o PIB da cidade\n");
+    scanf("%f", &pib);
+
+    //cadastro do ponto turistico
+    printf("Entre com a quantidade de pontos turisticos\n");
+    scanf("%i", &pontosTuristicos);
+
+    //decidi replicar os mesmos codigos acima para fazer o cadastro da carta 2, pensei em imprimir a carta 01 e para reutilizar as variaveis que ja foram declaradas
+    //porem como são poucas variaveis, decidi fazer desta maneira, criando mais, o ideal seria ter um arquivo .txt para guardar as informações das cartas, ao invés
+    //de ficar consumindo memoria, por ser um programa leve, não tem muito problema, mas em um programa de larga escala, isso pode vir a comprometer o desempenho do programa
+
+    printf("Carta 02\n");
+
+    printf("Entre com o nome do Estado da carta: (uma letra de 'A' a 'H')\n");
+    scanf(" %c", &estado2);
+
+    printf("Entre com o codigo da carta: (letra do estado + numero de 01 a 04, como exemplo A04)\n");
+    scanf(" %s", codigoCarta2);
+    getchar();
+
+    printf("Entre com o nome da cidade\n");
+    fgets(nomeCidade2,50,stdin);
+    nomeCidade2[strcspn(nomeCidade2, "\n")] = 0;
+
+    printf("Entre com o numero de habitantes\n");
+    scanf("%i", &populacao2);
+
+    printf("Entre com a área em km quadrados\n");
+    scanf("%f", &area2);
+
+    printf("Entre com o PIB da cidade\n");
+    scanf("%f", &pib2);
+
+    printf("Entre com a quantidade de pontos turisticos\n");
+    scanf("%i", &pontosTuristicos2);
+
+    printf("CARTA 01: \n Estado: %c\n Código: %s\n Nome da Cidade: %s\n População: %i\n Área: %.2f\n PIB: %.2f\n Pontos Turisticos: %i\n\n\n", estado, codigoCarta, nomeCidade, populacao, area, pib, pontosTuristicos);
+
+    printf("CARTA 02: \n Estado: %c\n Código: %s\n Nome da Cidade: %s\n População: %i\n Área: %.2f\n PIB: %.2f\n Pontos Turisticos: %i", estado2, codigoCarta2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2);
+}
